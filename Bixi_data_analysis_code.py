7b1f01d38eadbe0e75e34d1ef57578fe2d56065f
Yufei-Li-2021 <84288294+Yufei-Li-2021@users.noreply.github.com>
2021-11-05 13:15:30 -0400

    Update Bixi_data_analysis_code.py

--- Bixi_data_analysis_code.py
+++ Bixi_data_analysis_code.py
@@ -3,13 +3,13 @@ import numpy as np
 import os
 
 # I first begin by loading the station info.
-stationInfo = pd.read_csv('C:\\Users\test\Desktop\Milad_project\BIXI\Data\biximontrealrentals2019-33ea73\Stations_2019.csv',encoding='utf8')
+stationInfo = pd.read_csv('C:\\Users\test\Desktop\Milad_project\BIXI\Data\biximontrealrentals2019-33ea73\Stations_2019.csv', encoding='utf8')
 
 fileList = os.listdir('C:\\Users\test\Desktop\Milad_project\BIXI\Data\biximontrealrentals2019-33ea73')
 # I'll use the directory listing list to more easily load the separate CSV files into one dataframe
 print(fileList)
 
-data = pd.concat([pd.read_csv('C:\\Users\test\Desktop\Milad_project\BIXI\Data\biximontrealrentals2019-33ea73\'+fileName,encoding='utf8')
+data = pd.concat([pd.read_csv('C:\\Users\test\Desktop\Milad_project\BIXI\Data\biximontrealrentals2019-33ea73\\' +fileName, encoding='utf8' )
                   for fileName in fileList[1:-1]],
                  sort=False,ignore_index=True)
 
